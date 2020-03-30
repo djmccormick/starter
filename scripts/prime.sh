91ce27086aab8fab578d254fe0ec020c18482b86
Dustin McCormick <djmccormick@users.noreply.github.com>
2020-03-30 14:44:01 -0400

    [issue-2] latest adjustments for graphile-migrate
    Signed-off-by: Dustin McCormick <djmccormick@users.noreply.github.com>

--- scripts/prime.sh
+++ scripts/prime.sh
@@ -2,7 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	create user project;
-	create database project owner project;
-	create database project_shadow owner project;
+	create user project_user;
+	create database project owner project_user;
+	grant all privileges on database project to project_user;
+	create database project_shadow owner project_user;
+	grant all privileges on database project_shadow to project_user;
 EOSQL
